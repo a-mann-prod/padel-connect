e445ddf9543b2e89205fccf97973b2eeb7ab2113
Gregoire <gregoire.amann@gmail.com>
2024-07-03 23:06:28 +0400

    fix level estimation form

--- app/hooks/useLevelEstimationSteps.tsx
+++ app/hooks/useLevelEstimationSteps.tsx
@@ -1,8 +1,19 @@
 import { CarouselStepProps } from '@/components/Carousel/CarouselStep'
 import { LevelEstimationForm } from '@/components/Forms/LevelEstimationForm/LevelEstimationForm'
+import { ScrollView } from '@/designSystem'
 import { useTranslate } from '@/services/i18n'
+import { iterate } from '@/utils/array'
 
 export type ChoiceType = 'offense' | 'defense' | 'service'
+
+const useCreateChoices = () => {
+  const t = useTranslate('onboarding', { keyPrefix: 'levelEstimation' })
+  return (questionNb: number, nb: number) =>
+    iterate(nb).map((n) => ({
+      label: t(`question${questionNb}.choice${n}`),
+      value: n + 1,
+    }))
+}
 
 type Question = Omit<CarouselStepProps, 'containerProps' | 'content'> & {
   type: ChoiceType
@@ -13,46 +24,43 @@ export const useLevelEstimationSteps = (
   onSubmit: (value: number, type: ChoiceType) => void
 ): CarouselStepProps[] => {
   const t = useTranslate('onboarding', { keyPrefix: 'levelEstimation' })
+  const createChoices = useCreateChoices()
 
   const questions: Question[] = [
     {
       title: t('question0.title'),
       type: 'service',
-      choices: [
-        { label: t('question0.choice0'), value: 0 },
-        { label: t('question0.choice1'), value: 5 },
-        { label: t('question0.choice2'), value: 10 },
-      ],
+      choices: createChoices(0, 7),
     },
     {
       title: t('question1.title'),
       type: 'defense',
       subtitle: t('question1.subtitle'),
-      choices: [
-        { label: t('question1.choice0'), value: 0 },
-        { label: t('question1.choice1'), value: 5 },
-        { label: t('question1.choice2'), value: 10 },
-      ],
+      choices: createChoices(1, 7),
     },
     {
       title: t('question2.title'),
       type: 'defense',
       subtitle: t('question2.subtitle'),
-      choices: [
-        { label: t('question2.choice0'), value: 0 },
-        { label: t('question2.choice1'), value: 5 },
-        { label: t('question2.choice2'), value: 10 },
-      ],
+      choices: createChoices(2, 7),
     },
     {
       title: t('question3.title'),
-      type: 'offense',
+      type: 'defense',
       subtitle: t('question3.subtitle'),
-      choices: [
-        { label: t('question3.choice0'), value: 0 },
-        { label: t('question3.choice1'), value: 5 },
-        { label: t('question3.choice2'), value: 10 },
-      ],
+      choices: createChoices(3, 7),
+    },
+    {
+      title: t('question4.title'),
+      type: 'offense',
+      subtitle: t('question4.subtitle'),
+      choices: createChoices(4, 7),
+    },
+    {
+      title: t('question5.title'),
+      type: 'offense',
+      subtitle: t('question5.subtitle'),
+      choices: createChoices(5, 7),
     },
   ]
 
@@ -60,13 +68,15 @@ export const useLevelEstimationSteps = (
     ...questions.map(({ choices, type, ...question }) => ({
       ...question,
       content: (
-        <LevelEstimationForm
-          onSubmit={(v) => onSubmit(Number(v.level), type)}
-          options={choices.map(({ label, value }) => ({
-            label,
-            value: value.toString(),
-          }))}
-        />
+        <ScrollView>
+          <LevelEstimationForm
+            onSubmit={(v) => onSubmit(Number(v.level), type)}
+            options={choices.map(({ label, value }) => ({
+              label,
+              value: value.toString(),
+            }))}
+          />
+        </ScrollView>
       ),
     })),
   ]
