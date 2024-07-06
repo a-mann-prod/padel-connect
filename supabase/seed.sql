e59b04dc01a574a16e3d9bc13eddeba8d8e2f2db
Gregoire <gregoire.amann@gmail.com>
2024-07-06 18:38:30 +0400

    fix seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,3 @@
 -- COMPLEXES
 INSERT INTO "public".complexes (name) VALUES ('4Padel Rouen');
-INSERT INTO "public".complexes (name) VALUES ('4Padel Rouen');
+INSERT INTO "public".complexes (name) VALUES ('4Padel Petit-Quevilly');
