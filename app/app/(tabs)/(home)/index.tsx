d8cbf4a92d2bb7a8391ad2bf3f2d37dcabb35571
Gregoire <gregoire.amann@gmail.com>
2024-09-03 14:01:14 +0200

    fix: home page

--- app/app/(tabs)/(home)/index.tsx
+++ app/app/(tabs)/(home)/index.tsx
@@ -1,6 +1,8 @@
 import {
   Box,
+  HStack,
   Image,
+  SafeAreaView,
   useColorMode,
   useToken,
   VStack,
@@ -9,8 +11,7 @@ import * as Notifications from 'expo-notifications'
 import { router } from 'expo-router'
 import { useEffect } from 'react'
 
-import { TileButton } from '@/designSystem'
-import { useHeaderButton } from '@/hooks/useHeaderButton'
+import { HeaderButton, TileButton } from '@/designSystem'
 import { useMe } from '@/hooks/useMe'
 import { useNavigationBadgeCount } from '@/hooks/useNavigationBadgeCount'
 import { useUnreadNotifications } from '@/services/api'
@@ -36,54 +37,65 @@ export default () => {
     if (badgeCount !== null) Notifications.setBadgeCountAsync(badgeCount)
   }, [badgeCount])
 
-  useHeaderButton(
-    [
-      {
-        icon: 'FAS-bell',
-        onPress: () => router.navigate(routing.homeNotifications.path()),
-        badgeCount,
-        condition: !!me,
-      },
-    ],
-    'headerRight'
-  )
-
   useNavigationBadgeCount(badgeCount)
 
   return (
-    <Box flex={1}>
-      <Image
-        h="40%"
-        w="$full"
-        alt="home-header-background"
-        source={require('../../../assets/images/home-header-background.jpg')}
-        resizeMode="cover"
-      />
-      <Box
+    <>
+      <SafeAreaView
         flex={1}
-        mt={-30}
-        bgColor={backgroundColor}
-        borderTopLeftRadius="$3xl"
-        borderTopRightRadius="$3xl"
+        position="absolute"
+        bgColor="transparent"
+        zIndex={1}
+        w="$full"
       >
-        <VStack gap="$5" m="$5">
-          <TileButton
-            color="$white"
-            bgColor="$primary500"
-            title={t('navigation.tournaments')}
-            icon="FAS-trophy"
-            onPress={() => router.navigate(routing.homeTournaments.path())}
-          />
-          <TileButton
-            color="$white"
-            bgColor="$primary500"
-            title={t('navigation.myMatches')}
-            icon="FAS-baseball"
-            onPress={() => router.navigate(routing.homeMyMatches.path())}
-            disabled={!me}
-          />
-        </VStack>
+        <HStack mx="$5" justifyContent="flex-end">
+          {me && (
+            <Box bgColor="white" rounded="$full">
+              <HeaderButton
+                icon="FAS-bell"
+                onPress={() =>
+                  router.navigate(routing.homeNotifications.path())
+                }
+                badgeCount={badgeCount || null}
+              />
+            </Box>
+          )}
+        </HStack>
+      </SafeAreaView>
+      <Box flex={1}>
+        <Image
+          h="40%"
+          w="$full"
+          alt="home-header-background"
+          source={require('../../../assets/images/home-header-background.jpg')}
+          resizeMode="cover"
+        />
+        <Box
+          flex={1}
+          mt={-30}
+          bgColor={backgroundColor}
+          borderTopLeftRadius="$3xl"
+          borderTopRightRadius="$3xl"
+        >
+          <VStack gap="$5" m="$5">
+            <TileButton
+              color="$white"
+              bgColor="$yellow500"
+              title={t('navigation.tournaments')}
+              icon="FAS-trophy"
+              onPress={() => router.navigate(routing.homeTournaments.path())}
+            />
+            <TileButton
+              color="$white"
+              bgColor="$primary500"
+              title={t('navigation.myMatches')}
+              icon="FAS-baseball"
+              onPress={() => router.navigate(routing.homeMyMatches.path())}
+              disabled={!me}
+            />
+          </VStack>
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
