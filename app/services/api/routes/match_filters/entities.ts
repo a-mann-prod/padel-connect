505e707af7bdabdd0f5fb169444ac0afe6b88a9f
Gregoire <gregoire.amann@gmail.com>
2024-09-10 11:58:12 +0200

    fix: add missing supabase updates

--- app/services/api/routes/match_filters/entities.ts
+++ app/services/api/routes/match_filters/entities.ts
@@ -2,4 +2,5 @@ import { Tables } from '@/services/supabase/database.types'
 
 export type MatchFilterResponse = Tables<'match_filters'>
 
-export const matchFilterQueryCols = 'is_my_level_range, complex_id, created_at'
+export const matchFilterQueryCols =
+  'is_my_level_range, complex_id, user_id, created_at'
