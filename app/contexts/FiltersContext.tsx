8c104b1eb4f28fc79205ce7fd7d0defcd6502f88
Gregoire <gregoire.amann@gmail.com>
2024-09-20 17:54:48 +0200

    fix: remove toast on filter update

--- app/contexts/FiltersContext.tsx
+++ app/contexts/FiltersContext.tsx
@@ -1,7 +1,5 @@
 import { ReactNode, useEffect, useState } from 'react'
 
-import { useHandleError } from '@/hooks/useHandleError'
-import { useHandleSuccess } from '@/hooks/useHandleSuccess'
 import { useMe } from '@/hooks/useMe'
 import {
   UpdateMatchFilterParams,
@@ -27,19 +25,13 @@ export function FiltersProvider({ children }: { children: ReactNode }) {
   const [filters, setFilters] = useState<MatchFilters>({})
   const { data: me } = useMe()
 
-  const onSuccess = useHandleSuccess()
-  const onError = useHandleError()
-
   const { data: serverFilters, isLoading: isServerFiltersLoading } =
     useMatchFilter({
       params: { userId: me?.id as string },
       options: { enabled: !!me?.id },
     })
 
-  const { mutate: updateMatchFilter } = useUpdateMatchFilter({
-    onSuccess,
-    onError,
-  })
+  const { mutate: updateMatchFilter } = useUpdateMatchFilter()
 
   useEffect(() => {
     if (serverFilters) setFilters(serverFilters)
