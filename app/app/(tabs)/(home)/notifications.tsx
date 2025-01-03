49734f6072e8793cd96313d436f35e06fd3bf175
Gregoire <gregoire.amann@gmail.com>
2025-01-03 17:40:14 +0100

    fix: notifications listing

--- app/app/(tabs)/(home)/notifications.tsx
+++ app/app/(tabs)/(home)/notifications.tsx
@@ -14,6 +14,7 @@ import {
   useUnreadNotificationsCount,
 } from '@/services/api'
 import { NotificationType } from '@/services/api/types'
+import { useEffect, useMemo } from 'react'
 
 export default () => {
   const { data: me } = useMe()
@@ -28,48 +29,64 @@ export default () => {
     isRefetching,
   } = useInfiniteNotifications()
 
-  const notifications = notificationPages?.pages.reduce<
-    NotificationsResponse['results']
-  >((prev, acc) => [...prev, ...acc.results], [])
+  const notifications = useMemo(
+    () =>
+      notificationPages?.pages.reduce<NotificationsResponse['results']>(
+        (prev, acc) => [...prev, ...acc.results],
+        []
+      ),
+    [notificationPages?.pages]
+  )
 
-  const groupedNotifications = notifications?.reduce<
-    (NotificationsResponse['results'][number] & {
-      unread_ids: number[]
-    })[]
-  >((acc, curr) => {
-    const lastGroup = acc[acc.length - 1]
+  const groupedNotifications = useMemo(
+    () =>
+      notifications?.reduce<
+        (NotificationsResponse['results'][number] & {
+          unread_ids: number[]
+        })[]
+      >((acc, curr) => {
+        const lastGroup = acc[acc.length - 1]
 
-    if (
-      curr.type === NotificationType.NEW_MESSAGE &&
-      curr.associated_data?.url
-    ) {
-      // Si la notification est de type NEW_MESSAGE et qu'elle peut être regroupée
-      if (
-        lastGroup &&
-        lastGroup.type === NotificationType.NEW_MESSAGE &&
-        lastGroup.associated_data.url === curr.associated_data.url
-      ) {
-        // Ajoutez l'ID au tableau unread_ids uniquement si la notification n'a pas été lue
-        if (!curr.is_read) {
-          lastGroup.unread_ids = [...lastGroup.unread_ids, curr.id]
+        if (
+          curr.type === NotificationType.NEW_MESSAGE &&
+          curr.associated_data?.url
+        ) {
+          // Si la notification est de type NEW_MESSAGE et qu'elle peut être regroupée
+          if (
+            lastGroup &&
+            lastGroup.type === NotificationType.NEW_MESSAGE &&
+            lastGroup.associated_data.url === curr.associated_data.url
+          ) {
+            // Ajoutez l'ID au tableau unread_ids uniquement si la notification n'a pas été lue
+            if (!curr.is_read) {
+              lastGroup.unread_ids = [...lastGroup.unread_ids, curr.id]
+            }
+          } else {
+            // Sinon, créez un nouveau groupe et ajoutez l'ID si elle n'a pas été lue
+            acc.push({
+              ...curr,
+              unread_ids: curr.is_read ? [] : [curr.id],
+            })
+          }
+        } else {
+          // Pour les autres types de notifications, ajoutez-les directement
+          acc.push({
+            ...curr,
+            unread_ids: curr.is_read ? [] : [curr.id],
+          })
         }
-      } else {
-        // Sinon, créez un nouveau groupe et ajoutez l'ID si elle n'a pas été lue
-        acc.push({
-          ...curr,
-          unread_ids: curr.is_read ? [] : [curr.id],
-        })
-      }
-    } else {
-      // Pour les autres types de notifications, ajoutez-les directement
-      acc.push({
-        ...curr,
-        unread_ids: curr.is_read ? [] : [curr.id],
-      })
-    }
 
-    return acc
-  }, [])
+        return acc
+      }, []),
+    [notifications]
+  )
+
+  // force refetching to get mini 10 items for the 1st fetch
+  useEffect(() => {
+    if ((groupedNotifications?.length || 0) < 10 && hasNextPage) {
+      fetchNextPage()
+    }
+  }, [fetchNextPage, groupedNotifications, hasNextPage])
 
   const { mutate: readAllNotifications } = useReadAllNotifications()
 
