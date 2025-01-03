de8be02c81a233c3f054bda52563cbd5ef161da5
Gregoire <gregoire.amann@gmail.com>
2025-01-03 14:32:23 +0100

    feat: add default complex on match creation

--- app/app/match/(create)/index.tsx
+++ app/app/match/(create)/index.tsx
@@ -5,16 +5,20 @@ import {
   BookingMatchFieldFormValues,
 } from '@/components'
 import { useCreateMatchContext } from '@/contexts/CreateMatchContext'
+import { useMatchFilters } from '@/services/api'
 import { routing } from '@/services/routing'
 
 export default () => {
   const local = useLocalSearchParams()
   const datetime = local?.datetime as string | undefined
 
+  const { data: matchFilters } = useMatchFilters()
+
   const { setBookingFieldFormValues } = useCreateMatchContext()
 
   return (
     <BookingMatchFieldForm
+      complexId={matchFilters?.complex?.toString()}
       datetime={datetime}
       onSubmit={(data: BookingMatchFieldFormValues) => {
         setBookingFieldFormValues({
--- app/services/api/routes/me/match_filters/entities.ts
+++ app/services/api/routes/me/match_filters/entities.ts
@@ -1,8 +1,8 @@
 import { Entity, MatchType } from '@/services/api/types'
 
 export type MatchFiltersResponse = Entity<{
-  complex: number
+  complex?: number
   level_min: number
   level_max: number
-  type: MatchType
+  type?: MatchType
 }>
